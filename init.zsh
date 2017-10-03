9e0b9ddb817f1df36c08a625f2df67ba22849b48
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2017-10-03 15:30:55 +0100

    Add ssh config

--- init.zsh
+++ init.zsh
@@ -4,3 +4,6 @@ ln -s "$HOME/.config/shell/.zshenv" "$HOME/.zshenv"
 
 # .gitconfig
 ln -s "$HOME/.config/shell/gitconfig" "$HOME/.gitconfig"
+
+# .ssh/
+ln -s "$HOME/.config/shell/ssh/config" "$HOME/.ssh/config"
