a60960b1f53dbb38b8167d521db167b4c9807014
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2017-10-13 19:05:17 +0100

    Add ssh config for ecad-vm

--- init.sh
+++ init.sh
@@ -14,8 +14,3 @@ ln -s "$HOME/.dotfiles/gitconfig" "$HOME/.gitconfig"
 # .ssh/
 ln -s "$HOME/.dotfiles/ssh/config" "$HOME/.ssh/config"
 ln -s "$HOME/.dotfiles/ssh/keys" "$HOME/.ssh/authorized_keys"
-
-mkdir -p "$HOME/.config" "$HOME/.config/nvim"
-
-ln -s "$HOME/.dotfiles/tmux.conf" "$HOME/.tmux.conf"
-ln -s "$HOME/.dotfiles/vimrc "$HOME/.config/nvim/init.vim"
