15e62213db47fd820d3efb98161165de2ea84936
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2017-10-19 12:11:46 +0100

    Add relevant init lines for new files

--- init.sh
+++ init.sh
@@ -14,3 +14,9 @@ ln -s "$HOME/.dotfiles/gitconfig" "$HOME/.gitconfig"
 # .ssh/
 [[ $UID -ne 0 ]] && ln -s "$HOME/.dotfiles/ssh/config" "$HOME/.ssh/config"
 [[ $UID -ne 0 ]] && ln -s "$HOME/.dotfiles/ssh/keys" "$HOME/.ssh/authorized_keys"
+
+# tmux - Not currently used
+# ln -s "$HOME/.dotfiles/tmux.conf" "$HOME/.tmux.conf"
+
+# npmrc - Move .npm folder to cache
+ln -s "$HOME/.dotfiles/npmrc" "$HOME/.npmrc"
