c09f0fef4b44c81bdde0f80cdfae07959ae15609
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-07-13 00:19:39 +0100

    Add system Xresources file for use by lightdm

--- root.zsh
+++ root.zsh
@@ -5,3 +5,8 @@
 sudo rm -f /root/.dotfiles
 sudo -s ln -sT $HOME/.dotfiles /root/.dotfiles
 sudo /root/.dotfiles/modman install zsh
+
+# Link Xresources for lightdm colour swap script
+sudo rm -f /etc/Xresources
+echo "Symlink Xresources"
+sudo -s ln -s $HOME/.dotfiles/xrdb/Xresources /etc/Xresources
