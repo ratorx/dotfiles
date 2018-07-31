4ed16c36b312e5d781169f90cae3c22471425fd6
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-07-31 19:25:32 +0100

    Improve git interactive log script

--- git/log.sh
+++ git/log.sh
@@ -1,15 +1,17 @@
-#! /usr/bin/zsh
+#! /bin/bash
 # Git Commit Browser (w/ previews)
-_gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+_gitLogLineToHash="echo {} | grep -o '[a-f0-9]\\{7\\}' | head -1"
 _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
 
 glog() {
     git log --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr% C(auto)%an" "$@" |
-        fzf --no-sort --reverse --tiebreak=index --no-multi \
-        --ansi --preview $_viewGitLogLine \
-        --header "enter to view, alt-y to copy hash" \
-        --bind "enter:execute:$_viewGitLogLine   | less -R" \
-        --bind "alt-y:execute:$_gitLogLineToHash | xclip -sel clip"
+    fzf --no-sort --reverse --tiebreak=index --no-multi --ansi --exit-0 --preview "$_viewGitLogLine" | 
+    grep -o '[a-f0-9]\{7\}' | 
+    tr -d '\n' | 
+    xclip -i -sel clip
+   
+    # Send through exit code
+    e=$? && [ $e -eq 130 ] || return $e
 }
 
 glog "$@"
