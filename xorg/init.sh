52d7c9f0a55a69801cbe685fc485e62e7483b1b5
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-12-03 23:48:59 +0000

    Update autologin service

--- xorg/init.sh
+++ xorg/init.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 echo "Override getty@tty1 with autologin service file"
-sudo -s ln -s "$HOME/.dotfiles/xorg/autologin.conf" /etc/systemd/system/getty@tty1.service.d/override.conf
+# Symlink does not work, need to copy
+sudo -s cp "$HOME/.dotfiles/xorg/autologin.conf" /etc/systemd/system/getty@tty1.service.d/override.conf
