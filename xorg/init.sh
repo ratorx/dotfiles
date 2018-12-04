b29262e2de14ef1cc8a3ab0e84c0022c46be9365
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-12-04 00:01:43 +0000

    Add mkdir -p to xorg init script

--- xorg/init.sh
+++ xorg/init.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 echo "Override getty@tty1 with autologin service file"
 # Symlink does not work, need to copy
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 sudo -s cp "$HOME/.dotfiles/xorg/autologin.conf" /etc/systemd/system/getty@tty1.service.d/override.conf
