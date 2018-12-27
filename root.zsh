034e709eff545f1080170f29109e19e7931303e3
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-12-27 07:58:10 +0530

    Remove root Xresources symlink since lightdm is not used anymore

--- root.zsh
+++ root.zsh
@@ -5,8 +5,3 @@
 sudo rm -f /root/.dotfiles
 sudo -s ln -sT $HOME/.dotfiles /root/.dotfiles
 sudo /root/.dotfiles/modman install zsh
-
-# Link Xresources for lightdm colour swap script
-sudo rm -f /etc/Xresources
-echo "Symlink Xresources"
-sudo -s ln -s $HOME/.dotfiles/xrdb/Xresources /etc/Xresources
