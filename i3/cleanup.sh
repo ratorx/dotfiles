1a43a9a1a2fbc6c11c801b40b0c86d25d60e2b87
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-12-27 17:34:06 +0530

    Add brillo for backlight control and automation script

--- i3/cleanup.sh
+++ i3/cleanup.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 set -ex
-sudo systemctl disable i3lock.service
-sudo rm /etc/systemd/system/i3lock.service
 
+function service_uninstall() {
+    sudo systemctl disable "$1.service"
+}
+
+function is_laptop() {
+    for filename in /sys/class/power_supply/*/type; do
+        if [ "$(cat "$filename")" = "Battery" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+
+service_uninstall i3lock
+
+if is_laptop; then
+    service_uninstall backlight
+    sudo rm /etc/udev/rules.d/backlight.rules
+fi
--- i3/init.sh
+++ i3/init.sh
@@ -1,4 +1,25 @@
 #! /bin/bash
+MODULE_DIR="$HOME/.dotfiles/i3"
 set -ex
-sudo -s ln -s "$HOME/.dotfiles/i3/i3lock.service" /etc/systemd/system
-sudo systemctl enable i3lock.service
+
+function service_install() {
+    sudo -s ln -s "$MODULE_DIR/services/$1.service" /etc/systemd/system
+    sudo systemctl enable "$1.service"
+}
+
+function is_laptop() {
+    for filename in /sys/class/power_supply/*/type; do
+        if [ "$(cat "$filename")" = "Battery" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+service_install i3lock
+
+if is_laptop; then
+    service_install backlight
+    sudo -s ln -s "$MODULE_DIR/udev/backlight.rules" /etc/udev/rules.d
+fi
