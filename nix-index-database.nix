aa3e19bb3e8564ef0d411f2fd5cf95512f119499
Reeto Chatterjee <reeto@google.com>
2022-06-07 23:26:18 +0100

    Add support for nix-index-database on Mac

--- nix-index-database.nix
+++ nix-index-database.nix
@@ -1,46 +1,66 @@
-{ config, lib, pkgs, ...}:
+{ config, lib, pkgs, ... }:
 
-with lib;
+{
+  options.services.nix-index-database = {
+    enable = lib.mkEnableOption "Automatically update the nix-index database";
 
-let cfg = config.services.nix-index-database;
-defaultUpstreamPath = https://github.com/Mic92/nix-index-database/releases/latest/download;
-in {
-  options = {
-    services.nix-index-database = {
-      enable = mkEnableOption "Automatically update the nix-index database";
-
-      upstreamPath = mkOption {
-        type = types.str;
-        default = defaultUpstreamPath;
-        example = defaultUpstreamPath;
-        description = ''
-          The upstream URL to fetch the index database from.
-        '';
-      };
+    upstreamPath = lib.mkOption rec {
+      type = lib.types.str;
+      default = https://github.com/Mic92/nix-index-database/releases/latest/download;
+      example = default;
+      description = ''
+        The upstream URL to fetch the index database from.
+      '';
     };
   };
 
-  config = mkIf cfg.enable {
-    systemd.user.services.nix-index-database = {
-      Unit = { Description = "Update the nix-index database cache"; };
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.writeShellScript "nix-index-database-fetch.sh" ''
-          set -euo pipefail
-          PATH="${makeBinPath [ pkgs.coreutils pkgs.wget ]}"
+  config =
+    let
+      cfg = config.services.nix-index-database;
+      nix-index-database = "${pkgs.writeShellScript "nix-index-database-fetch.sh" ''
+          set -exuo pipefail
+          PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.wget ]};
           filename="index-$(uname -m)-$(uname | tr '[:upper:]' '[:lower:]')"
           mkdir -p "$XDG_CACHE_HOME/nix-index"
           cd "$XDG_CACHE_HOME/nix-index"
-          wget -nv -N "${strings.escapeShellArg cfg.upstreamPath}/$filename"
+          wget -nv -N ${lib.strings.escapeShellArg (builtins.toString cfg.upstreamPath)}/"$filename"
           ln -f "$filename" files
         ''}";
-      };
-    };
+    in
+    lib.mkMerge [
+      (lib.mkIf (cfg.enable && pkgs.stdenv.hostPlatform.isLinux) {
+        systemd.user.services.nix-index-database = {
+          Unit = { Description = "Update the nix-index database cache"; };
+          Service = {
+            Type = "oneshot";
+            ExecStart = nix-index-database;
+          };
+        };
 
-    systemd.user.timers.nix-index-database = {
-      Unit = { Description = "Update the nix-index database cache"; };
-      Timer = { OnCalendar = "daily"; };
-      Install = { WantedBy = ["timers.target"]; };
-    };
-  };
+        systemd.user.timers.nix-index-database = {
+          Unit = { Description = "Update the nix-index database cache"; };
+          Timer = { OnCalendar = "daily"; };
+          Install = { WantedBy = [ "timers.target" ]; };
+        };
+      })
+      (lib.mkIf (cfg.enable && pkgs.stdenv.hostPlatform.isDarwin) {
+        #   Unit = { Description = "Update the nix-index database cache"; };
+        launchd.agents."nix-index-database" = {
+          enable = true;
+          config = {
+            Program = nix-index-database;
+            StartCalendarInterval = [{
+              Hour = 5;
+            }];
+            StandardOutPath = "/tmp/nix-index.log";
+            StandardErrorPath = "/tmp/nix-index.err.log";
+            EnvironmentVariables = {
+              XDG_CACHE_HOME = config.xdg.cacheHome;
+              # Use system certs
+              SSL_CERT_FILE = "/etc/ssl/cert.pem";
+            };
+          };
+        };
+      })
+    ];
 }
