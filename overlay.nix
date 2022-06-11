fd0fdd055bb6d0e2d007b6f7ef0ea95d0fd554e7
Reeto Chatterjee <git@ree.to>
2022-06-11 11:49:05 +0100

    Format shellUtil better

--- overlay.nix
+++ overlay.nix
@@ -6,11 +6,16 @@ let
 in
 {
   custom = {
-    shellUtil = ({src, deps ? [], extraEnv ? "", impure ? false}: pkgs.writeShellScriptBin (lib.strings.removeSuffix ".sh" (builtins.baseNameOf src)) ''
-      PATH=${lib.makeBinPath deps}${if impure then ":$PATH" else ""}
-      ${extraEnv}
+    shellUtil = ({ src
+                 , deps ? [ ]
+                 , extraEnv ? ""
+                 , impure ? false
+                 }:
+      pkgs.writeShellScriptBin (lib.strings.removeSuffix ".sh" (builtins.baseNameOf src)) ''
+        PATH=${lib.makeBinPath deps}${if impure then ":$PATH" else ""}
+        ${extraEnv}
 
-      ${builtins.readFile src}
-    '');
+        ${builtins.readFile src}
+      '');
   };
 }
