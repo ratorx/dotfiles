623b06d8166589bd65015907ea660a5a8508bb3e
Reeto Chatterjee <git@ree.to>
2022-06-11 11:47:54 +0100

    Add explicit dependencies to all flake inputs

--- base.nix
+++ base.nix
@@ -52,6 +52,15 @@
       deps = [ pkgs.coreutils pkgs.direnv ];
       impure = true;
     })
+    # This is an amazing hack that makes 'n' and 'nman' work offline if the
+    # package is already present! This is necessary since there's no way for Nix
+    # to track dependencies in flake inputs (as they don't usually reference
+    # each other by path). Without this, all flake deps of the packages (i.e.
+    # this flake) would be fetched (and cleaned up by nix-collect-garbage).
+    # This WILL NOT WORK (I think) if there are dependent flakes.
+    (pkgs.writeTextDir "inputs" ''
+      ${builtins.toString (builtins.attrValues inputs)}
+    '')
   ];
   home.sessionVariables = rec {
     LESSHISTFILE = "/dev/null";
