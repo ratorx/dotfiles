1722262455b3a104ca2a17918e0927ce38dec630
Reeto Chatterjee <git@ree.to>
2022-12-21 20:04:30 +0000

    Configure language servers for Go and Rust

--- vim/lspconfig.lua
+++ vim/lspconfig.lua
@@ -1,9 +1,14 @@
 local lsp = require('lsp')
 local lspconfig = require('lspconfig')
 
+-- TODO: Investigate language servers for:
+-- * Python (maybe ruff + black + ??? for completions)
+-- * Svelte (tsserver + sveltels + tailwindcssls + ???)
 local cfgs = {
+  gopls = {}, -- Go
   hls = {}, -- Haskell
   rnix = {}, -- Nix
+  rust_analyzer = {},-- Rust
   sumneko_lua = {}, -- Lua
 }
 
