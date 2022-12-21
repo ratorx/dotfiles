ba08a757932b9834e35241ce4451939f132c58b6
Reeto Chatterjee <git@ree.to>
2022-12-21 19:27:49 +0000

    Move Lua settings to .luarc.json
    This sacrifices a bit on features, because Lua can no longer get
    completions from runtime Neovim. However, this lets Lua be used in other
    projects without as much hassle.

--- vim/lspconfig.lua
+++ vim/lspconfig.lua
@@ -2,18 +2,9 @@ local lsp = require('lsp')
 local lspconfig = require('lspconfig')
 
 local cfgs = {
-  hls = {},
-  rnix = {},
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        runtime = { version = 'LuaJIT' },
-        diagnostics = { globals = { 'vim' }, disable = { 'unused-function' } },
-        workspace = { library = vim.api.nvim_get_runtime_file('', true), checkThirdParty = false },
-        telemetry = { enable = false },
-      },
-    },
-  },
+  hls = {}, -- Haskell
+  rnix = {}, -- Nix
+  sumneko_lua = {}, -- Lua
 }
 
 for name, cfg in pairs(cfgs) do
