7a9bd26e2e6219e2cc7285d01faf5998105d72b8
Reeto Chatterjee <me@ree.to>
2023-12-14 01:34:49 +0000

    Add iterm2 shell integration on Mac

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 # NOTE: Be careful about using variables in this file because the shell
 # needs to be restarted to use it.
 {
@@ -32,7 +32,15 @@
       # Google
       gssh = "/usr/local/bin/rw --check_remaining";
     };
-    interactiveShellInit = (builtins.readFile ./interactive.fish);
+    interactiveShellInit = lib.mkMerge [
+      (builtins.readFile ./interactive.fish)
+      (lib.mkIf pkgs.stdenv.isDarwin ''
+        set -l iterm "/Applications/iTerm.app/Contents/Resources/iterm2_shell_integration.fish"
+        if test -f $iterm
+          source $iterm
+        end
+      '')
+    ];
   };
 
   # WARNING: Attempting to re-create this shim in ZSH is futile, because path_helper (which is called in the profile stage), will re-order the PATH set in env stage. So MacOS devices are better off settting Bash as the user shell and not complicating it even more.
