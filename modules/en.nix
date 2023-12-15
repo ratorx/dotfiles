e3cdb0b69dbea97ddfd4059cba2b10d72e2858ad
Reeto Chatterjee <me@ree.to>
2023-12-15 23:41:39 +0000

    Only keep n in minimal variant

--- modules/en.nix
+++ modules/en.nix
@@ -1,21 +1,25 @@
-{ inputs, flakeRoot, pkgs, ... }:
+{ config, flakeRoot, inputs, lib, pkgs, ... }:
 
 {
   imports = [ inputs.nix-index-database.hmModules.nix-index ];
-  home.packages = [
-    (pkgs.custom.builder.n flakeRoot)
-    (pkgs.custom.builder.nman flakeRoot)
-    pkgs.custom.pkgfile
-    pkgs.custom.pkgbin
-    # This is an amazing hack that makes 'n' and 'nman' work offline if the
-    # package is already present! This is necessary since there's no way for Nix
-    # to track dependencies in flake inputs (as they don't usually reference
-    # each other by path). Without this, all flake deps of the packages (i.e.
-    # this flake) would be fetched (and cleaned up by nix-collect-garbage).
-    # This WILL NOT WORK (I think) if there are dependent flakes.
-    (pkgs.writeTextDir "inputs" ''
-      ${builtins.toString (builtins.attrValues inputs)}
-    '')
+  home.packages = lib.mkMerge [
+    [
+      (pkgs.custom.builder.n flakeRoot)
+      # This is an amazing hack that makes 'n' and 'nman' work offline if the
+      # package is already present! This is necessary since there's no way for Nix
+      # to track dependencies in flake input sources (as they don't usually reference
+      # each other by path). Without this, all flake deps of the packages (i.e.
+      # this flake) would be fetched (and cleaned up by nix-collect-garbage).
+      # This WILL NOT WORK (I think) if there are dependent flakes.
+      (pkgs.writeTextDir "inputs" ''
+        ${builtins.toString (builtins.attrValues inputs)}
+      '')
+    ]
+    (lib.mkIf (!config.variants.minimal) [
+      (pkgs.custom.builder.nman flakeRoot)
+      pkgs.custom.pkgfile
+      pkgs.custom.pkgbin
+    ])
   ];
   programs.nix-index.enable = true;
 }
